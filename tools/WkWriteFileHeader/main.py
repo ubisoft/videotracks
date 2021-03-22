ca78d8d8211851c806d323361000e73900d01b18
julien blervaque <julien.blervaque@ubisoft.com>
2021-03-22 17:05:30 +0100

    Add GNU licence header to source files

---
+++ tools/WkWriteFileHeader/main.py
@@ -0,0 +1,7 @@
+from wkwritefileheader import prepend_text_to_file
+
+
+# dir = input("Enter a directory: ")
+dir = "Z:/EvalSofts/Blender/DevPython/UAS_VideoTracks_Addon/videotracks/"
+prepend_text_to_file(dir, "py", "tools\WkWriteFileHeader\gnu_licence.txt")
+
