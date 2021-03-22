8e1a7cd91fcf317479c7c99d4b0c8be907808b81
julien blervaque <julien.blervaque@ubisoft.com>
2021-03-22 17:06:54 +0100

    Delete temp files

--- tools/WkWriteFileHeader/main.py
+++
@@ -1,7 +0,0 @@
-from wkwritefileheader import prepend_text_to_file
-
-
-# dir = input("Enter a directory: ")
-dir = "Z:/EvalSofts/Blender/DevPython/UAS_VideoTracks_Addon/videotracks/"
-prepend_text_to_file(dir, "py", "tools\WkWriteFileHeader\gnu_licence.txt")
-
